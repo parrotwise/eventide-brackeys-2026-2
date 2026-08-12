129b826d5564ffdcc81f7c4070318f7be39fcf3c
Parrotwise <parrotwisearts@gmail.com>
2026-08-12 20:43:47 +0300

    Refactor audio code structure

---
+++ objects/audio/sfx_player.gd
@@ -0,0 +1,11 @@
+class_name SFXPlayer
+extends AudioStreamPlayer
+
+
+func play_clip(clip_name: StringName, volume: float = 0.5) -> void:
+	volume_linear = volume
+	
+	if not playing:
+		play()
+
+	get_stream_playback().switch_to_clip_by_name(clip_name)
--- objects/global/audio.gd
+++ objects/global/audio.gd
@@ -1,39 +1,20 @@
 extends Node
 
 
-var sfx_player: PackedScene = preload('res://objects/audio/sfx_player.tscn')
-var music_player: PackedScene = preload('res://objects/audio/music_player.tscn')
+var sfx_player_template: PackedScene = preload('res://objects/audio/sfx_player.tscn')
+var music_player_template: PackedScene = preload('res://objects/audio/music_player.tscn')
 
-
-func _ready() -> void:
-	add_child(sfx_player.instantiate())
-	add_child(music_player.instantiate())
+@onready var sfx_player: SFXPlayer = sfx_player_template.instantiate()
+@onready var music_player: MusicPlayer = music_player_template.instantiate()
 
 
 func play_sfx(clip_name: StringName, volume: float = 0.5) -> void:
-	if has_node(^'SFXPlayer'):
-		_start_player($SFXPlayer, clip_name, volume)
+	sfx_player.play_clip(clip_name, volume)
 
 
 func play_music(clip_name: StringName, volume: float = 0.5) -> void:
-	if has_node(^'MusicPlayer'):
-		_start_player($MusicPlayer, clip_name, volume)
+	music_player.play_clip(clip_name, volume)
 
 
 func stop_music() -> void:
-	if has_node(^'MusicPlayer'):
-		_stop_player($MusicPlayer)
-
-
-func _start_player(asp: AudioStreamPlayer, clip_name: StringName, volume: float = 0.5) -> void:
-	asp.volume_linear = volume
-	
-	if not asp.playing:
-		asp.play()
-
-	asp.get_stream_playback().switch_to_clip_by_name(clip_name)
-
-
-func _stop_player(asp: AudioStreamPlayer) -> void:
-	if asp.playing:
-		asp.stop()
+	music_player.stop()
