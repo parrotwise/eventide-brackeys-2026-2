65fc51711ec7c95cc90a2fc9b4ffd1ed50f2c79d
Elliot <elliot@mozstro.com>
2026-08-23 21:04:45 +0100

    actions and health change

---
+++ objects/game/components/actions_component.gd
@@ -0,0 +1,5 @@
+class_name ActionsComponent
+extends Node
+
+
+@export var actions: Array[Resource] = []
--- objects/game/components/health_component.gd
+++ objects/game/components/health_component.gd
@@ -1,23 +1,40 @@
 class_name HealthComponent
 extends Node
 
+signal health_changed(current_health: int, max_health: int)
+signal knockout
 
-signal knockout()
+@export var max_health: int = 100
 
-@export var max_health: int
+var current_health: int
+var knocked_out: bool = false
 
-var character: Player
 
-@onready var current_health: int = max_health
-@onready var knocked_out: bool = false
+func _ready() -> void:
+	current_health = max_health
 
 
 func take_damage(damage: int) -> void:
 	if knocked_out:
 		return
-	
+
 	current_health = maxi(0, current_health - damage)
+	health_changed.emit(current_health, max_health)
 
 	if current_health == 0:
 		knocked_out = true
 		knockout.emit()
+
+
+func heal(amount: int) -> void:
+	if knocked_out:
+		return
+
+	current_health = mini(max_health, current_health + amount)
+	health_changed.emit(current_health, max_health)
+
+
+func reset_health() -> void:
+	current_health = max_health
+	knocked_out = false
+	health_changed.emit(current_health, max_health)
