f36fe11d77f07083768d78f8248e9174fcb8ada3
Elliot <elliot@mozstro.com>
2026-08-24 23:51:30 +0100

    Action resource updates

--- objects/game/actions/action.gd
+++ objects/game/actions/action.gd
@@ -5,16 +5,20 @@ extends Resource
 signal used(user: Character, target: Character)
 
 
-@export_category("Effects")
+@export_group("Identifiers")
+
+@export var name: String
+@export_multiline() var description: String
+@export var icon: Texture
+
+@export_group("Effect Definition")
 
 @export var damage: int = 0
 @export var healing: int = 0
 
 @export_category("Targeting")
 
-@export var range: float = 500.0
-
-var character: Character
+@export var range_type: Enums.RangeType = Enums.RangeType.MELEE
 
 
 func use(user: Character, target: Character) -> void:
