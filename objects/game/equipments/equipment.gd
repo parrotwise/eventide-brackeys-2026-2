7c0304409e0213fdaa4194f590ac29d192641351
Jack Costa-Kuswanto <44042705+JackCostaK@users.noreply.github.com>
2026-08-24 17:41:18 -0400

    Implemented Equipment Resource

--- objects/game/equipments/equipment.gd
+++ objects/game/equipments/equipment.gd
@@ -1,12 +1,12 @@
 class_name Equipment
 extends Resource
 
-
+@export_group("Identifiers")
 ## A name to be exposed to the player.
 @export var name: String
 ## A description to be exposed to the player.
 @export_multiline() var description: String
 ## An icon to be exposed to the player.
-@export var icon: Texture
+@export var icon: Texture2D
 ## Implement the persistent effect of using this Equipment as a Status effect.
 @export var equipped_status: Status
