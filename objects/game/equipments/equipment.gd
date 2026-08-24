ada14799a59384d324941d629a7a97bc3507dc3c
Parrotwise <parrotwisearts@gmail.com>
2026-08-24 11:13:42 +0300

    Refactor Equipment equipped effect definition into Status, enforce Equipment to use a unique Status to define its equipped effect

--- objects/game/equipments/equipment.gd
+++ objects/game/equipments/equipment.gd
@@ -1,19 +1,12 @@
 class_name Equipment
 extends Resource
 
-@export_group("Required vars")
+
 ## A name to be exposed to the player.
 @export var name: String
 ## A description to be exposed to the player.
 @export_multiline() var description: String
-
-
-@export_group("Optional vars")
-## Value to be added to the user's attack. Negative values subtract attack.
-@export var attack_adder: float = 0
-## Value to be added to the user's health. Negative values subtract health.
-@export var health_adder: float = 0
-## Allows the user to attack twice in one turn.
-@export var can_attack_twice: bool = false
-## An array of StatusEffect resources.
-@export var status_effects: Array[StatusEffect] = []
+## An icon to be exposed to the player.
+@export var icon: Texture
+## Implement the persistent effect of using this Equipment as a Status effect.
+@export var equipped_status: Status
--- objects/game/statuses/status.gd
+++ objects/game/statuses/status.gd
@@ -2,7 +2,7 @@ class_name Status
 extends Resource
 
 
-@export_group("Required vars")
+@export_group("Identifiers")
 ## A name to be exposed to the player.
 @export var name: String
 ## A description to be exposed to the player.
@@ -10,5 +10,12 @@ extends Resource
 ## An icon to be exposed to the player.
 @export var icon: Texture
 
-@export_group("Optional vars")
 @export var vfx: PackedScene
+
+@export_group("Effect Definition")
+## Value to be added to the owner's attack. Negative values subtract attack.
+@export var attack_adder: float = 0
+## Value to be added to the owner's health. Negative values subtract health.
+@export var health_adder: float = 0
+## Allows the owner to attack twice in one turn.
+@export var can_attack_twice: bool = false
