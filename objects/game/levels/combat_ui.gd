4f87d2ef00f9402af72ec0405797c3dd0297d006
Chase <48299551+StoneHolder28@users.noreply.github.com>
2026-08-24 23:24:44 -0400

    revised combat_ui and updated placeholders
    Updated bottom panel ui node tree to match concept art/discussion.
    Updated background placeholder textures.
    Added particles to background.
    Updated combat_ui to preserve and update buttons based on character action resources.

--- objects/game/levels/combat_ui.gd
+++ objects/game/levels/combat_ui.gd
@@ -6,8 +6,12 @@ var top_panel: PanelContainer:
 	get: return $TopPanel
 var bottom_panel: PanelContainer:
 	get: return $BottomPanel
-var action_buttons_grid: GridContainer:
-	get: return $BottomPanel/ActionButtons
+var action_buttons_grid: HFlowContainer:
+	get: return $BottomPanel/BottomPanel/ActionButtons
+var passive_icon: TextureRect:
+	get: return $BottomPanel/BottomPanel/PassiveBG/PassiveIcon
+var equipment_buttons_grid: HFlowContainer:
+	get: return $BottomPanel/BottomPanel/EquipmentButtons
 var action_buttons: Array[TextureButton]:
 	get: return Array(
 		action_buttons_grid.get_children(),
@@ -19,13 +23,19 @@ var pause_menu: Control:
 
 func set_action_buttons(character: Character) -> void:
 	for button: TextureButton in action_buttons:
-		button.queue_free()
+		button.hide()
 	
-	for action: Action in character.actions:
-		var button := TextureButton.new()
-
-		## TODO: Configure button
-		# button.name = '%sButton' % [action.name]
-		# button.texture_normal = action.icon
-
-		action_buttons_grid.add_child(button)
+	if character.actions.size() > 4:
+		Debug.error("Character '%s' has too many actions (more than 4)." % character.name, Debug.Verbosity.CALLER)
+	
+	for action_index: int in character.actions.size():
+		var button: TextureButton = action_buttons[action_index]
+		var action_icon: TextureRect = button.get_child(0)
+		
+		button.show()
+		
+		if action_icon:
+			action_icon.texture = character.actions[action_index].icon
+		
+		var action_tooltip: String = character.actions[action_index].name + "\n" + character.actions[action_index].description
+		button.tooltip_text = action_tooltip
