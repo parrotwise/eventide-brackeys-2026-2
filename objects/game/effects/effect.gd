7cf29294e9d9987ced8d25b3255c8e2c31b8938a
Parrotwise <parrotwisearts@gmail.com>
2026-08-26 12:59:05 +0300

    Implement Effect resource

---
+++ objects/game/effects/effect.gd
@@ -0,0 +1,28 @@
+class_name Effect
+extends Resource
+
+
+signal applied()
+
+@export_group("Effect Definition")
+
+@export var damage: int = 0
+@export var healing: int = 0
+@export var applied_statuses: Array[Status] = []
+
+var target: Character
+
+
+func apply(_bypass_queue: bool = false) -> void:
+	if not target:
+		return
+	
+	## Immediate effects first
+	target.state_component.take_damage(damage)
+	target.state_component.heal(healing)
+	
+	## Persistent effects next
+	for status: Status in applied_statuses:
+		target.state_component.apply_status(status)
+	
+	applied.emit()
