e95e50c1f49b4709e65b9e3ba6d92c99ba0d124c
Parrotwise <parrotwisearts@gmail.com>
2026-08-27 17:26:13 +0300

    Implement new action button template with dedicated script and plug&play texture layers support

---
+++ objects/ui/action_button.gd
@@ -0,0 +1,55 @@
+class_name ActionButton
+extends Control
+
+
+signal button_down()
+signal button_up()
+signal pressed()
+signal toggled(toggled_on: bool)
+
+
+@export var icon_texture: Texture2D
+@export var frame_texture: Texture2D
+@export var inner_texture_normal: Texture2D
+@export var inner_texture_pressed: Texture2D
+@export var inner_texture_hover: Texture2D
+@export var inner_texture_disabled: Texture2D
+@export var inner_texture_selected_normal: Texture2D
+@export var inner_texture_selected_pressed: Texture2D
+@export var inner_texture_selected_hover: Texture2D
+@export var background_texture: Texture2D
+
+var icon: TextureRect:
+	get: return $Icon
+var frame: TextureRect:
+	get: return $Frame
+var button: TextureButton:
+	get: return $InnerButton
+var background: TextureRect:
+	get: return $Background
+
+
+func _ready() -> void:
+	button.button_down.connect(button_down.emit)
+	button.button_up.connect(button_up.emit)
+	button.pressed.connect(pressed.emit)
+	button.toggled.connect(toggled.emit)
+
+	icon.texture = icon_texture
+	frame.texture = frame_texture
+	background.texture = background_texture
+
+	set_selected(true)
+
+
+func set_selected(selected: bool = false):
+	if selected:
+		button.texture_normal = inner_texture_selected_normal
+		button.texture_pressed = inner_texture_selected_pressed
+		button.texture_hover = inner_texture_selected_hover
+	else:
+		button.texture_normal = inner_texture_normal
+		button.texture_pressed = inner_texture_pressed
+		button.texture_hover = inner_texture_hover
+	
+	button.texture_disabled = inner_texture_disabled
