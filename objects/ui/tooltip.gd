f65d7bdbde932be4158f9164e71ed9ae823ac8d8
Chase <48299551+StoneHolder28@users.noreply.github.com>
2026-08-28 11:54:47 -0400

    revised tooltip.gd
    Reworked NinePatchRect expansion, better incorporated direction selection.

--- objects/ui/tooltip.gd
+++ objects/ui/tooltip.gd
@@ -1,4 +1,4 @@
-extends Control
+extends NinePatchRect
 
 
 @export_enum("NE", "SE", "SW", "NW") var growth_direction: String = "NE"
@@ -7,19 +7,33 @@ extends Control
 @export var popup_delay_time: float = 1.0
 @export var text_speed: float = 100
 
-var nine_patch_rect: NinePatchRect:
-	get: return $NinePatchRect
+const open_close_margin: int = 2
+const full_margin: int = 38
+
+var parent: Control:
+	get:
+		if $".." is Control:
+			return $".."
+		else:
+			Debug.error("Tooltip parent is not derived from Control.", Debug.Verbosity.CALLER)
+			return
+var name_label: RichTextLabel:
+	get: return $MarginContainer/VBoxContainer/NameLabel
+var description_label: RichTextLabel:
+	get: return $MarginContainer/VBoxContainer/DescriptionLabel
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	hide()
+	name_label.text = ""
+	description_label.text = ""
 	
 	# TESTING
 	grow_tooltip(Vector2.ZERO)
 	hide()
 	await get_tree().create_timer(3).timeout
-	grow_tooltip(Vector2(800,380))
+	grow_tooltip(Vector2(500,380))
 	# TESTING
 
 
@@ -34,20 +48,39 @@ func _process(delta: float) -> void:
 
 
 func grow_tooltip(tooltip_size: Vector2) -> void:
+	patch_margin_top = open_close_margin
+	patch_margin_bottom = open_close_margin
 	show()
-	var move_to: Vector2
+	var move_to: Vector2 = Vector2.ZERO
 	match growth_direction:
 		"NE":
+			move_to.x = parent.size.x
 			move_to.y = -tooltip_size.y
+			position.x = parent.size.x
+			position.y = -size.y
 		"SE":
-			move_to = Vector2.ZERO
+			move_to = parent.size
+			position = parent.size
 		"SW":
-			move_to = -tooltip_size
-		"NW":
 			move_to.x = -tooltip_size.x
+			move_to.x = -parent.size.x
+			move_to.y = parent.size.y
+			position.x = -size.x
+			position.y = parent.size.y
+		"NW":
+			move_to = -tooltip_size
+			position = -size
 	
 	var tooltip_tween = create_tween()
 	tooltip_tween.tween_property(self, "size:x", tooltip_size.x, 0.2)
 	tooltip_tween.parallel().tween_property(self, "position:x", move_to.x, 0.2)
+	
 	tooltip_tween.tween_property(self, "size:y", tooltip_size.y, 0.3)
 	tooltip_tween.parallel().tween_property(self, "position:y", move_to.y, 0.3)
+	
+	tooltip_tween.tween_property(self, "patch_margin_top", full_margin, 0.15)
+	tooltip_tween.parallel().tween_property(self, "patch_margin_bottom", full_margin, 0.15)
+
+
+func fill_text() -> void:
+	pass
