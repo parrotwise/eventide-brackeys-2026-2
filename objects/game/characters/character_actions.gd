78e3cfc2b64b8ff7313f8027f5dfa5703615e5a8
Parrotwise <parrotwisearts@gmail.com>
2026-08-29 00:09:33 +0300

    Fix assigned Actions of a Character no longer properly instantiated since the basic attack / reposition implementation

--- objects/game/characters/character_actions.gd
+++ objects/game/characters/character_actions.gd
@@ -13,8 +13,18 @@ var character: Character
 
 
 func _ready() -> void:
+	Game.start.connect(_on_combat_start)
+
 	basic_attack = basic_attack.duplicate()
 	reposition = reposition.duplicate()
 	
-	for i: int in actions.size():
-		actions[i] = actions[i].duplicate()
+	for i: int in skills.size():
+		skills[i] = skills[i].duplicate()
+
+
+func _on_combat_start() -> void:
+	basic_attack.owner = character
+	reposition.owner = character
+
+	for skill: Action in skills:
+		skill.owner = character
