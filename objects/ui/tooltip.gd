0f282e787e41bf00e6501f40b0c9332ba1be0a96
Chase <48299551+StoneHolder28@users.noreply.github.com>
2026-08-28 17:06:19 -0400

    Reworked tooltip.tscn and tooltip.gd
    It will work until it doesn't. I am so sorry to anyone who has to come in here behind me, including myself.

--- objects/ui/tooltip.gd
+++ objects/ui/tooltip.gd
@@ -1,14 +1,27 @@
-extends NinePatchRect
+extends Control
 
-
-@export_enum("NE", "SE", "SW", "NW") var growth_direction: String = "NE"
+@export_group("Text")
 @export var header: String
 @export_multiline() var description: String
+@export var text_speed: float = 40
+@export var text_chunk_size: int = 7
+
+@export_group("NinePatchRect Vars")
+@export var nine_patch_texture: Texture2D
+@export_subgroup("Margins", "margin_")
+@export var margin_left: int = 45
+@export var margin_top: int = 40
+@export var margin_right: int = 45
+@export var margin_bottom: int = 40
+
+@export_group("")
+@export_enum("NE", "SE", "SW", "NW") var growth_direction: String = "NE"
 @export var popup_delay_time: float = 1.0
-@export var text_speed: float = 100
 
 const open_close_margin: int = 2
 const full_margin: int = 38
+
+var is_expanded: bool = false
 
 var parent: Control:
 	get:
@@ -17,70 +30,153 @@ var parent: Control:
 		else:
 			Debug.error("Tooltip parent is not derived from Control.", Debug.Verbosity.CALLER)
 			return
+var hover_timer: Timer:
+	get: return $HoverTimer
+var background: NinePatchRect:
+	get: return $NinePatchRect
 var name_label: RichTextLabel:
-	get: return $MarginContainer/VBoxContainer/NameLabel
+	get: return $NinePatchRect/MarginContainer/VBoxContainer/NameLabel
 var description_label: RichTextLabel:
-	get: return $MarginContainer/VBoxContainer/DescriptionLabel
+	get: return $NinePatchRect/MarginContainer/VBoxContainer/DescriptionLabel
+var hover_detection: Control:
+	get: return self
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	hide()
-	name_label.text = ""
-	description_label.text = ""
+	z_index = 256
 	
-	# TESTING
-	grow_tooltip(Vector2.ZERO)
-	hide()
-	await get_tree().create_timer(3).timeout
-	grow_tooltip(Vector2(500,380))
-	# TESTING
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-# TODO: Determine tooltip size by input text size?? Think if necessary when refreshed.
-# TODO: Add and integrate timer.
-# TODO: Add scaling mouse event detection (Full Rect no-alpha button?)
+	hide_components()
+	shrink_tooltip()
+	
+	hover_detection.size = parent.size
+	hover_timer.wait_time = popup_delay_time
+	
+	if nine_patch_texture:
+		background.texture = nine_patch_texture
+	background.patch_margin_left = margin_left
+	background.patch_margin_top = margin_top
+	background.patch_margin_right = margin_right
+	background.patch_margin_bottom = margin_bottom
 
 
 func grow_tooltip(tooltip_size: Vector2) -> void:
-	patch_margin_top = open_close_margin
-	patch_margin_bottom = open_close_margin
-	show()
+	# Make sure the text is hidden to prevent odd behavior.
+	name_label.text = ""
+	description_label.text = ""
+	
+	background.patch_margin_top = open_close_margin
+	background.patch_margin_bottom = open_close_margin
+	background.patch_margin_left = full_margin
+	background.patch_margin_right = full_margin
+	
+	show_components()
+	
 	var move_to: Vector2 = Vector2.ZERO
 	match growth_direction:
 		"NE":
 			move_to.x = parent.size.x
 			move_to.y = -tooltip_size.y
-			position.x = parent.size.x
-			position.y = -size.y
+			background.position.x = parent.size.x
+			background.position.y = 0
 		"SE":
 			move_to = parent.size
-			position = parent.size
+			background.position = parent.size
 		"SW":
 			move_to.x = -tooltip_size.x
-			move_to.x = -parent.size.x
 			move_to.y = parent.size.y
-			position.x = -size.x
-			position.y = parent.size.y
+			background.position.x = -size.x
+			background.position.y = parent.size.y
 		"NW":
 			move_to = -tooltip_size
-			position = -size
+			background.position = -size
 	
 	var tooltip_tween = create_tween()
-	tooltip_tween.tween_property(self, "size:x", tooltip_size.x, 0.2)
-	tooltip_tween.parallel().tween_property(self, "position:x", move_to.x, 0.2)
+	tooltip_tween.tween_property(background, "size:x", tooltip_size.x, 0.2)
+	tooltip_tween.parallel().tween_property(background, "position:x", move_to.x, 0.2)
 	
-	tooltip_tween.tween_property(self, "size:y", tooltip_size.y, 0.3)
-	tooltip_tween.parallel().tween_property(self, "position:y", move_to.y, 0.3)
+	tooltip_tween.tween_property(background, "size:y", tooltip_size.y, 0.3)
+	tooltip_tween.parallel().tween_property(background, "position:y", move_to.y, 0.3)
 	
-	tooltip_tween.tween_property(self, "patch_margin_top", full_margin, 0.15)
-	tooltip_tween.parallel().tween_property(self, "patch_margin_bottom", full_margin, 0.15)
+	tooltip_tween.tween_property(background, "patch_margin_top", full_margin, 0.15)
+	tooltip_tween.parallel().tween_property(background, "patch_margin_bottom", full_margin, 0.15)
+	
+	# Now that the box is the right size, populate it with text.
+	await tooltip_tween.finished
+	is_expanded = true
+	fill_text()
+
+
+func shrink_tooltip() -> void:
+	remove_text()
+	
+	var move_to: Vector2 = Vector2.ZERO
+	match growth_direction:
+		"NE":
+			move_to.x = parent.size.x
+			#move_to.y = -tooltip_size.y
+		"SE":
+			move_to = parent.size
+		"SW":
+			#move_to.x = -tooltip_size.x
+			move_to.x = -parent.size.x
+			move_to.y = parent.size.y
+		"NW":
+			#move_to = -tooltip_size
+			background.position = -size
+	
+	var tooltip_tween = create_tween()
+	tooltip_tween.tween_property(background, "size:y", 2 * open_close_margin, 0.3)
+	tooltip_tween.parallel().tween_property(background, "position:y", move_to.y, 0.3)
+	
+	tooltip_tween.tween_property(background, "size:x", 0, 0.2)
+	tooltip_tween.parallel().tween_property(background, "position:x", move_to.x, 0.2)
+	
+	await tooltip_tween.finished
+	background.patch_margin_top = 0
+	background.patch_margin_bottom = 0
+	background.patch_margin_left = 0
+	background.patch_margin_right = 0
+	
+	hide_components()
+	is_expanded = false
 
 
 func fill_text() -> void:
-	pass
+	for chunk in range(0, header.length() + text_chunk_size, text_chunk_size):
+		name_label.text = header.substr(0, chunk)
+		await get_tree().create_timer(1/text_speed).timeout
+	for chunk in range(0, description.length() + text_chunk_size, text_chunk_size):
+		description_label.text = description.substr(0, chunk)
+		await get_tree().create_timer(1/text_speed).timeout
+
+
+func remove_text() -> void:
+	name_label.text = ""
+	description_label.text = ""
+
+
+func hide_components() -> void:
+	for child in get_children():
+		if child.has_method("hide"):
+			child.hide()
+	size = parent.size
+	position = Vector2.ZERO
+
+func show_components() -> void:
+	for child in get_children():
+		if child.has_method("show"):
+			child.show()
+
+
+func _on_hover_detection_mouse_entered() -> void:
+	hover_timer.start()
+
+
+func _on_hover_detection_mouse_exited() -> void:
+	hover_timer.stop()
+	shrink_tooltip()
+
+
+func _on_hover_timer_timeout() -> void:
+	grow_tooltip(Vector2(500,320))
