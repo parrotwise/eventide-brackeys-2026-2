47229de285078f46e4d13a57cb695b9996b46684
Parrotwise <parrotwisearts@gmail.com>
2026-08-29 22:33:53 +0300

    Fix bug where CombatSelector remained unresponsive at the turn of the round, and the targeting mode was not being reset

--- objects/game/levels/combat_level/combat_level.gd
+++ objects/game/levels/combat_level/combat_level.gd
@@ -149,6 +149,7 @@ func _on_action_finished(
 			target.name
 		]
 	)
+	selector_component.cancel_action()
 	turn_tracker_component.end_current_turn()
 	
 	
@@ -231,6 +232,8 @@ func _on_round_started(round_number: int) -> void:
 
 func _on_battle_group_started(group_name: StringName) -> void:
 	Debug.debug("-- %s phase!" % group_name)
+	if group_name == turn_tracker_component.ALLIES_GROUP:
+		selector_component.select_ally(characters.ally_melee, false)
 
 
 func _on_turn_started(character: Character) -> void:
