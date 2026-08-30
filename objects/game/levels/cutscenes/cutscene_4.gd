2c980e7052bf06d4a03e846182206ffbbad64f63
Chase <48299551+StoneHolder28@users.noreply.github.com>
2026-08-30 02:25:45 -0400

    uhhhh
    added logo card for startup
    added cutscene_4.tscn
    added character selection in equipment_level.tscn

---
+++ objects/game/levels/cutscenes/cutscene_4.gd
@@ -0,0 +1,7 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	await get_tree().create_timer(10).timeout
+	TransitionLayer.transition_betrayal(TransitionLayer.combat_level)
