3779b7d6d40ca1f0587908b34cdeb1c7b511bcfc
Chase <48299551+StoneHolder28@users.noreply.github.com>
2026-08-29 22:35:45 -0400

    polished cutscene3, added betrayal transition

--- objects/global/transition_layer.gd
+++ objects/global/transition_layer.gd
@@ -1,20 +1,27 @@
 extends CanvasLayer
 
 
+signal transition_finished()
+
+
 #const intro_narrative: String = ""
 #const betrayal_narrative: String = ""
 const equipment_level: String = "res://objects/game/levels/equipment_level/equipment_level.tscn"
+const cutscene_3: String = "res://objects/game/levels/cutscenes/cutscene3.tscn"
 const combat_level: String = "res://objects/game/levels/combat_level/combat_level.tscn"
 
 var blindfold: ColorRect:
 	get: return $ColorRect
+var betrayal: Control:
+	get: return $Betrayal
 
 
 func _ready() -> void:
 	blindfold.modulate.a = 0
+	betrayal.hide()
 
 
-func simple_fade(scene_path: String) -> void:
+func transition_simple_fade(scene_path: String) -> void:
 	var transition_tween = create_tween()
 	transition_tween.tween_property(blindfold, "modulate:a", 1, 0.2)
 	
@@ -26,3 +33,17 @@ func simple_fade(scene_path: String) -> void:
 	await get_tree().create_timer(0.1).timeout
 	var new_transition_tween = create_tween()
 	new_transition_tween.tween_property(blindfold, "modulate:a", 0, 0.2)
+	new_transition_tween.parallel().tween_property(betrayal, "modulate:a", 0, 0.2)
+	
+	transition_finished.emit()
+
+
+func transition_betrayal(scene_path: String) -> void:
+	betrayal.modulate.a = 0
+	betrayal.show()
+	
+	var tween = create_tween().set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_OUT)
+	tween.tween_property(betrayal, "modulate:a", 1, 0.2)
+	
+	await tween.finished
+	transition_simple_fade(scene_path)
