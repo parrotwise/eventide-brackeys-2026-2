284ed3e00cca9efe17a238bafb42cd5d3ffed72b
Jake Eves <jakeseves@gmail.com>
2026-09-03 19:11:47 +0100

    Added AkEvent2D posting example to Wwise test scene

--- temp/jake/wwise_testing.gd
+++ temp/jake/wwise_testing.gd
@@ -4,8 +4,10 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	Wwise.post_event("Ability_RC", self)
+	$AkEvent2D.post_event()
+	#Wwise.post_event("Ability_RC", self)
 	$Button.pressed.connect(test_wwise)
-
+	
+	
 func test_wwise() -> void:
 	event.post(self)
