aaf06b203fcdfb00026268ce1f514f0dd9c8da86
Parrotwise <parrotwisearts@gmail.com>
2026-09-04 20:55:49 +0300

    Add Audio interface template in prep for Wwise migration

--- objects/global/audio.gd
+++ objects/global/audio.gd
@@ -1,6 +1,28 @@
 extends Node
 
 
+## TODO: Add one item per Wwise event.
+enum Event {
+	NONE,
+	WWISE_EVENT1,
+	WWISE_EVENT2,
+}
+
+## TODO: Add one item per Wwise StateGroup+State combination.
+enum State {
+	NONE,
+	WWISE_STATE1,
+	WWISE_STATE2,
+}
+
+## TODO: Add one item per Wwise SwitchGroup.
+enum Switch {
+	NONE,
+	WWISE_SWITCH1,
+	WWISE_SWITCH2,
+}
+
+## TODO: Deprecated! Replace with the enums above.
 enum Clip {
 	NONE,
 	BC_ABILITY_CHARGE,
@@ -25,6 +47,7 @@ enum Clip {
 	UI_BUTTON,
 }
 
+## TODO: Deprecated! Replace with the enums above.
 enum Track {
 	TRACK1,
 	TRACK2,
@@ -34,15 +57,18 @@ var master_volume: float = 0.5
 var music_volume: float = 0.5
 var sfx_volume: float = 0.5
 
+var audio_components: Array[CharacterAudio] = []
+
+## TODO: Both deprecated! Remove after Wwise migration.
 var sfx_player_template: PackedScene = preload('res://objects/audio/sfx_player.tscn')
 var music_player_template: PackedScene = preload('res://objects/audio/music_player.tscn')
 
-var audio_components: Array[CharacterAudio] = []
-
+## TODO: Both deprecated! Remove after Wwise migration.
 var sfx_player: SFXPlayer
 var music_player: MusicPlayer
 
 
+## TODO: Deprecated! Remove/Replace after Wwise migration.
 func _ready() -> void:
 	sfx_player = sfx_player_template.instantiate()
 	music_player = music_player_template.instantiate()
@@ -63,22 +89,84 @@ func _process(_delta: float) -> void:
 	)
 
 
+func trigger_event(event: Event) -> void:
+	var event_name: String = _event_name(event)
+	if event_name:
+		Wwise.post_event(event_name, self)
+
+
+func set_state(state: State) -> void:
+	var state_group: String = _state(state)['group']
+	var state_value: String = _state(state)['value']
+	if state_group:
+		Wwise.set_state(state_group, state_value)
+
+
+func set_switch(switch: Switch, switch_value: String) -> void:
+	var switch_group: String = _switch_group(switch)['group']
+	if switch_group:
+		Wwise.set_switch(switch_group, switch_value, self)
+
+
+## TODO: Deprecated! Replaced with the functions above.
 func play_sfx(clip: Clip, volume: float = 0.5) -> void:
 	var clip_name: StringName = _pick_clip(clip)
 	if clip_name:
 		sfx_player.play_clip(clip_name, volume)
 
 
+## TODO: Deprecated! Replaced with the functions above.
 func play_music(track: Track, volume: float = 0.5) -> void:
 	var track_name: StringName = _pick_track(track)
 	if track_name:
 		music_player.play_clip(track_name, volume)
 
 
+## TODO: Deprecated! Replaced with the functions above.
 func stop_music() -> void:
 	music_player.stop()
 
 
+func _event_name(event: Event) -> String:
+	match event:
+		Event.WWISE_EVENT1:
+			return 'WwiseEvent1Name'
+		Event.WWISE_EVENT2:
+			return 'WwiseEvent2Name'
+	
+	return ''
+
+
+func _state(state: State) -> Dictionary[String, String]:
+	match state:
+		State.WWISE_STATE1:
+			return {
+				'group': 'WwiseState1GroupName',
+				'value': 'WwiseState1Name'
+			}
+		State.WWISE_STATE2:
+			return {
+				'group': 'WwiseState2GroupName',
+				'value': 'WwiseState2Name'
+			}
+	
+	return {
+		'group': '',
+		'value': ''
+	}
+
+
+func _switch_group(switch: Switch) -> String:
+	match switch:
+		Switch.WWISE_SWITCH1:
+			return 'WwiseSwitch1GroupName'
+		Switch.WWISE_SWITCH2:
+			return 'WwiseSwitch2GroupName'
+	
+	return ''
+
+
+## TODO: Deprecated! Replaced with the functions above.
 func _pick_clip(clip: Clip) -> StringName:
 	match clip:
 		Clip.BC_ABILITY_CHARGE:
@@ -125,6 +213,7 @@ func _pick_clip(clip: Clip) -> StringName:
 	return &''
 
 
+## TODO: Deprecated! Replaced with the functions above.
 func _pick_track(track: Track) -> StringName:
 	match track:
 		Track.TRACK1:
