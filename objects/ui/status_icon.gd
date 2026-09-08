5f4356396fe0d4ca8236ee2ee7489f8f90faecce
Parrotwise <parrotwisearts@gmail.com>
2026-09-08 21:21:08 +0300

    Add self-updating stack number label in StatusIcons

--- objects/ui/status_icon.gd
+++ objects/ui/status_icon.gd
@@ -6,6 +6,8 @@ var icon: TextureRect:
 	get: return $Icon
 var button: TextureButton:
 	get: return $InnerButton
+var stack_label: RichTextLabel:
+	get: return $StackLabel
 
 var status: Status
 
@@ -13,6 +15,18 @@ var tooltip_header: String:
 	get: return status.name if status else ''
 var tooltip_description: String:
 	get: return status.description if status else ''
+
+
+func _process(_delta: float) -> void:
+	if not status:
+		return
+	
+	if status.stack == 1:
+		stack_label.hide()
+	
+	else:
+		stack_label.text = '%d' % [status.stack]
+		stack_label.show()
 
 
 func setup(new_status: Status) -> void:
